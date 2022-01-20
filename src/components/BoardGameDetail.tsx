8f452cdefa26a5f400f1943253d87757a39da6c2
Ashley Wright <ashleyelainewright@gmail.com>
2022-01-20 13:42:37 -0600

    Some cleanup

--- src/components/BoardGameDetail.tsx
+++ src/components/BoardGameDetail.tsx
@@ -36,21 +36,6 @@ const BoardGameDetail = (props: { boardgame: IBoardGame }) => {
           </Grid>
         </Grid>
       </Box>
-
-      // <div className="boardgame">
-      //   <div className="title">
-      //     <img width="120" src={boardgame.image_url} alt={boardgame.name} />
-      //     <h2>{boardgame.name}</h2>
-      //     <p><b>Price:</b> ${boardgame.price}</p>
-      //     <p><small><b>Published:</b> {boardgame.year_published}</small></p>
-      //     <details>
-      //       <summary>Description</summary>
-      //       <p>{boardgame.description_preview}</p>
-      //     </details>
-      //   </div>
-
-      //   <a href={boardgame.official_url} target="_bla">More&nbsp;&raquo;</a>
-      // </div>
     )
 };
 
